b2341f18facaa0db5191e0ece6ef8c8b23236980
chenxiangyu <chenxiangyu@qutoutiao.net>
2019-04-25 16:45:33 +0800

    。。。。

--- src/condition/XyLike.php
+++ src/condition/XyLike.php
@@ -5,7 +5,7 @@ namespace XiangYu2038\Wish\Condition;
 
 class XyLike extends XyCondition
 {
-    public function __construct($expressions)
+    public function __construct($expressions=[])
     {
         parent::__construct($expressions);
         $this ->setType('like');
